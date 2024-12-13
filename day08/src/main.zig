5543dc964603064da41c364c5c81fc6fabb68dd2
Marcel Heistermann <mheistermann@gmx.de>
2024-12-13 14:00:55 +0100

    store height and width of the "city" as well

--- day08/src/main.zig
+++ day08/src/main.zig
@@ -9,38 +9,57 @@ const Antenna = struct {
     freq: u8,
 };
 
-fn parse_input(a: std.mem.Allocator, input: []const u8) !std.ArrayList(Antenna) {
-    var res = std.ArrayList(Antenna).init(a);
-
-    var line_it = utils.enumerate(std.mem.splitScalar(u8, input, '\n'));
-    while (line_it.next()) |line| {
-        if (line.val.len == 0) break; // trailing newline
-        for (0.., line.val) |col, ch| {
-            if (ch == '.') continue;
-            try res.append(
-                .{ .row = @intCast(line.idx), .col = @intCast(col), .freq = ch },
-            );
-        }
+const City = struct {
+    width: u32,
+    height: u32,
+    antennas: []Antenna,
+    a: std.mem.Allocator,
+    pub fn deinit(self: *@This()) void {
+        self.a.free(self.antennas);
     }
-
-    return res;
-}
+    fn init_from_input(a: std.mem.Allocator, input: []const u8) !City {
+        var res = City{
+            .a = a,
+            .width = undefined,
+            .height = undefined,
+            .antennas = undefined,
+        };
+        var antennas = std.ArrayList(Antenna).init(a);
+        var line_it = utils.enumerate(std.mem.splitScalar(u8, input, '\n'));
+        res.width = @intCast(line_it.peek().?.val.len);
+        while (line_it.next()) |line| {
+            res.height = @intCast(line.idx + 1);
+            if (line.val.len == 0) break; // trailing newline
+            for (0.., line.val) |col, ch| {
+                if (ch == '.') continue;
+                try antennas.append(
+                    .{ .row = @intCast(line.idx), .col = @intCast(col), .freq = ch },
+                );
+            }
+        }
+        res.antennas = try antennas.toOwnedSlice();
+        return res;
+    }
+};
 
 test "parse_input" {
+    const expectEq = std.testing.expectEqual;
     const expectDeq = std.testing.expectEqualDeep;
     const a = std.testing.allocator;
-    const res = try parse_input(a,
+    var res = try City.init_from_input(a,
         \\............
         \\........0...
         \\.....r......
     );
     defer res.deinit();
+    try expectEq(3, res.height);
+    try expectEq(12, res.width);
     try expectDeq(
         &.{
             Antenna{ .row = 1, .col = 8, .freq = '0' },
             Antenna{ .row = 2, .col = 5, .freq = 'r' },
         },
-        res.items,
+        res.antennas,
     );
 }
 
@@ -69,7 +88,7 @@ pub fn main() !void {
     defer file.close();
 
     const input = try file.readToEndAlloc(a, std.math.pow(u32, 2, 20));
-    const data = try parse_input(a, input);
+    const data = try City.init_from_input(a, input);
 
     std.log.debug("Calculatig sum", .{});
     const sum: u64 = data.items.len;
--- utils/src/iter.zig
+++ utils/src/iter.zig
@@ -6,11 +6,19 @@ fn EnumeratingIterator(comptime TIter: type) type {
         inner: TIter,
         const TOpt = @typeInfo(@TypeOf(TIter.next)).@"fn".return_type orelse ?void;
         const T = std.meta.Child(TOpt);
+        pub inline fn first(self: *@This()) struct { idx: usize, val: T } {
+            const idx = self.idx;
+            self.idx += 1;
+            return .{ .idx = idx, .val = self.inner.first() };
+        }
         pub inline fn next(self: *@This()) ?struct { idx: usize, val: T } {
             const idx = self.idx;
             const val = self.inner.next() orelse return null;
             self.idx += 1;
             return .{ .idx = idx, .val = val };
+        }
+        pub inline fn peek(self: *@This()) ?struct { idx: usize, val: T } {
+            return .{ .idx = 0, .val = self.inner.peek() orelse return null };
         }
     };
 }
