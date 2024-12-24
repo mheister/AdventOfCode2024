feca3cad2f2f0313ec30889eed2150efdd464e48
Marcel Heistermann <mheistermann@gmx.de>
2024-12-24 15:03:02 +0100

    implement bxc instruction

--- day17/src/computer.zig
+++ day17/src/computer.zig
@@ -12,6 +12,7 @@ pub const Computer = struct {
     ip: usize = 0,
     program: []const u8,
     printer: ?Printer = null,
+
     pub fn step(self: *@This()) Error!StepResult {
         if (self.ip >= self.program.len) {
             return .halted;
@@ -23,14 +24,17 @@ pub const Computer = struct {
             1 => try self.bxl(fetch[1]),
             2 => try self.bst(fetch[1]),
             3 => try self.jnz(fetch[1]),
+            4 => try self.bxc(fetch[1]),
             5 => try self.out(fetch[1]),
             else => return error.InvalidOpcode,
         }
         return .ok;
     }
+
     pub fn setPrinter(self: *@This(), p: Printer) void {
         self.printer = p;
     }
+
     fn combo(self: *const @This(), operand: u8) Error!usize {
         return switch (operand) {
             0...3 => @intCast(operand),
@@ -40,6 +44,7 @@ pub const Computer = struct {
             else => error.InvalidCombo,
         };
     }
+
     test "combo" {
         const c = @This(){ .reg_a = 10, .reg_b = 20, .reg_c = 30, .program = &.{} };
         try expectEq(0, try c.combo(0));
@@ -50,22 +55,31 @@ pub const Computer = struct {
         try expectEq(20, try c.combo(5));
         try expectEq(30, try c.combo(6));
     }
+
     fn adv(self: *@This(), operand: u8) Error!void {
         const operand_resolved = try self.combo(operand);
         self.reg_a = self.reg_a / (@as(usize, 1) << @intCast(operand_resolved));
     }
+
     fn bxl(self: *@This(), operand: u8) Error!void {
         self.reg_b = self.reg_b ^ operand;
     }
+
     fn bst(self: *@This(), operand: u8) Error!void {
         const operand_resolved = try self.combo(operand);
         self.reg_b = operand_resolved & 0b111;
     }
+
     fn jnz(self: *@This(), operand: u8) Error!void {
         if (self.reg_a != 0) {
             self.ip = @intCast(operand);
         }
     }
+
+    fn bxc(self: *@This(), _: u8) Error!void {
+        self.reg_b = self.reg_b ^ self.reg_c;
+    }
+
     fn out(self: *@This(), operand: u8) Error!void {
         if (self.printer) |printer| {
             const operand_resolved = try self.combo(operand);
@@ -185,6 +199,15 @@ test "jnz" {
         c.reg_a = 11;
         _ = try c.step();
         try expectEq(8, c.ip);
+    }
+}
+
+test "bxc" {
+    {
+        const program = [_]u8{ 4, 0 };
+        var c = Computer{ .reg_b = 0b10101, .reg_c = 0b11110, .program = &program };
+        _ = try c.step();
+        try expectEq(0b01011, c.reg_b);
     }
 }
 
